f7c04da89f5339aa6cc98bd59364b4885a7cfa2b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2026-08-24 08:39:24 +0900

    add:ECSキャパシティプロバイダーの作成

--- ecs.tf
+++ ecs.tf
@@ -8,3 +8,15 @@ resource "aws_ecs_cluster" "app" {
     value = "disabled"
   }
 }
+
+# ECSクラスターのキャパシティプロバイダー
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster_capacity_providers
+resource "aws_ecs_cluster_capacity_providers" "app" {
+  cluster_name = aws_ecs_cluster.app.name
+  capacity_providers =  ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight =  1
+  }
+}
